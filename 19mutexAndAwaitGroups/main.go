4fef2d886fa328168e61bbf95828439c43165616
Yash Agrawal <agarwalshivam834@gmail.com>
2025-03-20 02:10:34 +0530

    further explaination in channels

--- 19mutexAndAwaitGroups/main.go
+++ 19mutexAndAwaitGroups/main.go
@@ -13,31 +13,31 @@ func main() {
 
 	var score = []int{0}
 
-	wg.Add(3)
+	wg.Add(2)
+
+	go func(w *sync.WaitGroup, m *sync.Mutex) {
+		fmt.Println("Two R")
+		// m.Lock()
+		m.Unlock()
+		score = append(score, 2)
+		wg.Done()
+	}(wg, mut)
 
 	go func(w *sync.WaitGroup, m *sync.Mutex) {
 		fmt.Println("One R")
 		m.Lock()
 		score = append(score, 1)
-		m.Unlock()
+		// m.Unlock()
 		wg.Done()
 	}(wg, mut)
 
-	go func(w *sync.WaitGroup, m *sync.Mutex) {
-		fmt.Println("Two R")
-		m.Lock()
-		score = append(score, 2)
-		m.Unlock()
-		wg.Done()
-	}(wg, mut)
-
-	go func(w *sync.WaitGroup, m *sync.Mutex) {
-		fmt.Println("Three R")
-		m.Lock()
-		score = append(score, 3)
-		m.Unlock()
-		wg.Done()
-	}(wg, mut)
+	// go func(w *sync.WaitGroup, m *sync.Mutex) {
+	// 	fmt.Println("Three R")
+	// 	m.Lock()
+	// 	score = append(score, 3)
+	// 	m.Unlock()
+	// 	wg.Done()
+	// }(wg, mut)
 
 	wg.Wait()
 
--- 20channels/main.go
+++ 20channels/main.go
@@ -5,10 +5,15 @@ import (
 	"sync"
 )
 
+// https://chatgpt.com/share/67db2ab2-eab8-8007-b5c0-4346a86ba6a5 (at the end)
+
+// channels are needed for:
+// 1. communication between the channels
+// 2. synchronization for execution (can also use time.sleep and wait groups)
 func main() {
 	fmt.Println("Channels in Go")
 
-	myCh := make(chan int, 2)
+	myCh := make(chan int, 1) // it means we can take one extra value in addition to normal one so in this case if we send two values in channel, atleast one needs to be recieved
 	wg := &sync.WaitGroup{}
 
 	// myCh <- 5
@@ -22,6 +27,7 @@ func main() {
 
 		fmt.Println(isChannelOpen)
 		fmt.Println(val)
+		fmt.Println("working fine")
 
 		wg.Done()
 	}(myCh, wg)
